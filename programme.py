4985cfb42c76c43f43d8362e133cd4da2580ec97
spiderLambo <spiderLambo.dev@outlook.com>
2024-03-26 20:19:20 +0100

    v1

--- programme.py
+++ programme.py
@@ -1,7 +1,49 @@
 from random import *
 
-# création de la grille
-grille = [[randint(0, 1) for i in range(16)] for i in range(16)]
+# Taille du plateau
+HEIGH = 16
+WIDTH = 16
 
+# création de la grille
+grille = [[choice((True, False)) for i in range(WIDTH)] for i in range(HEIGH)]
+
+
+# * Tentatives pour avoir tout ce quil y as autours
+liste = []
+
+for i in range(len(grille)):
+    for j in range(len(grille[i])):
+        somme = []
+        somme.append(grille[i - 1][j - 1])
+        somme.append(grille[i - 1][j])
+        try:
+            somme.append(grille[i - 1][j + 1])
+        except:
+            somme.append(grille[i - 1][0])
+
+        somme.append(grille[i][j - 1])
+        somme.append(grille[i][j])
+        try:
+            somme.append(grille[i][j + 1])
+        except:
+            somme.append(grille[i][0])
+
+        try:
+            somme.append(grille[i + 1][j - 1])
+            somme.append(grille[i + 1][j])
+            try:
+                somme.append(grille[i + 1][j + 1])
+            except:
+                somme.append(grille[i + 1][0])
+        except:
+            somme.append(grille[0][j - 1])
+            somme.append(grille[0][j])
+            try:
+                somme.append(grille[0][j + 1])
+            except:
+                somme.append(grille[0][0])
+
+        liste.append(somme)
 
 print(grille)
+print(liste[0])
