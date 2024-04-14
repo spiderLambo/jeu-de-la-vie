e88b977433b03edfc2a2ddba981645c0395092a0
spiderLambo <spiderLambo.dev@outlook.com>
2024-04-14 12:20:53 +0200

    lenia cellules non discretes

--- funcLENIA.py
+++ funcLENIA.py
@@ -1,3 +1,11 @@
+# Fonction qui revoie la nouvelle valeur en fonction du nombre de voisins
+def lenia_graph(number):
+    if 0.26 < number < 1.74:
+        return -0.9 * number * number + 1.8 * number - 0.1
+    else:
+        return 0.3
+
+
 def voisins(x, y, grille):
     nb = 0  # Initalisation du nombre de voisins
     # Calcul des dimentions de la grille
@@ -16,7 +24,7 @@ def voisins(x, y, grille):
         + grille[(y + 1) % largeur][(x - 1) % hauteur]  # diagonale bas-droite
     )
 
-    return nb
+    return lenia_graph(nb)
 
 
 def new_grid(grille):
@@ -29,29 +37,8 @@ def new_grid(grille):
     # Parcourir les cellules
     for y in range(len(grille)):
         for x in range(len(grille[0])):
+            # Nouvelle valeur
             alentours = voisins(x, y, grille)
-            # Règle 1 - Mort d'isolement
-            if (
-                grille[y][x] == 1 and alentours < 2
-            ):  # Si la cellule est vivante et qu'elle a un nombre de voisins inférieur à deux
-                nouvelle_grille[y][x] = 0  # alors elle meurt
-
-            # Règle 2 - Toute cellule avec 2 ou 3 voisins survit.
-            if grille[y][x] == 1 and (
-                alentours in [2, 3]
-            ):  # Si une cellule est vivante et qu'elle a deux ou trois voisins
-                nouvelle_grille[y][x] = 1  # alors elle reste en vie
-
-            # Règle 3 - Mort par surpopulation
-            if (
-                grille[y][x] == 1 and alentours > 3
-            ):  # si une cellule est vivante et qu'elle a plus de trois voisins
-                nouvelle_grille[y][x] = 0  # alors elle meurt
-
-            # Règle 4 - Naissance
-            if (
-                grille[y][x] == 0 and alentours == 3
-            ):  # si une cellule est morte et qu'elle a trois voisins
-                nouvelle_grille[y][x] = 1  # alors elle nait (son état est à vivant)
+            nouvelle_grille[y][x] = alentours
 
     return nouvelle_grille
