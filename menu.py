6402ca2af66afd27d2f2e6d1aae7679c70dc2db9
spiderLambo <spiderLambo.dev@outlook.com>
2024-04-14 17:42:57 +0200

    menu

--- menu.py
+++ menu.py
@@ -1,13 +1,10 @@
-from funcLENIA import *
+from func import *
 import pygame as pgm
 from random import *
 
-# dimentions, taille_pixels = int(input("Quelle dimentions : ")), int(
-#     input("Quelle taille de pixels : ")
-# )
-# grille = [[random() for i in range(dimentions)] for i in range(dimentions)]
 
 pgm.init()  # Initialiser la fenetre
+clock = pgm.time.Clock()
 
 
 screen = pgm.display.set_mode((300, 300))  # Afficher la fenetre
@@ -29,7 +26,7 @@ dim_rect = pgm.Rect(10, 100, 100, 30)
 taille_rect = pgm.Rect(10, 200, 100, 30)
 dimentions = ""
 taille_pixels = ""
-
+gen = arialFont.render(f"Generation 0", 1, (0, 0, 0))
 dim_active = False
 taille_active = False
 
@@ -45,14 +42,25 @@ while run:
             # Activer la fenetre des parametres
             if play_rect.collidepoint(event.pos):
                 parametre = True
+
             # Demarer la simulation
             if start_rect.collidepoint(event.pos):
+                dimentions = int(dimentions)
+                taille_pixels = int(taille_pixels)
                 jouer = True
+                grille = [
+                    [randint(0, 1) for i in range(dimentions)]
+                    for i in range(dimentions)
+                ]
             # Zones de textes
             if dim_rect.collidepoint(event.pos):
                 dim_active = True
             else:
                 dim_active = False
+            if taille_rect.collidepoint(event.pos):
+                taille_active = True
+            else:
+                taille_active = False
 
         if event.type == pgm.KEYDOWN:
             if dim_active:
@@ -62,17 +70,26 @@ while run:
                 else:
                     dimentions += event.unicode
 
+            if taille_active:
+                # Ecrire dans les zones de textes
+                if event.key == pgm.K_BACKSPACE:
+                    taille_pixels = taille_pixels[
+                        :-1
+                    ]  # Faire fonctionner la fleche retours
+                else:
+                    taille_pixels += event.unicode
+
             # Changer de grille
-            if event.key == pgm.K_RIGHT:
+            if event.type == pgm.KEYDOWN and event.key == pgm.K_RIGHT:
                 grille = new_grid(grille)
                 compteur += 1  # Incrementation du compteur
-                texte_compteur = arialFont.render(
+                gen = arialFont.render(
                     f"Generation {compteur}", 1, (0, 0, 0)
                 )  # Actualisation du compteur
 
     screen.fill((250, 250, 250))  # Changer la couleur de fond
 
-    if parametre:
+    if parametre and not jouer:
         # Dessin des textes
         screen.blit(dim_txt, (10, 50))
         screen.blit(taille_txt, (10, 150))
@@ -83,36 +100,92 @@ while run:
             dim_color = (100, 200, 255)
         else:
             dim_color = (100, 100, 100)
+        if taille_active:
+            taille_color = (100, 200, 255)
+        else:
+            taille_color = (100, 100, 100)
 
         # Dessin des zones de textes
         pgm.draw.rect(screen, dim_color, dim_rect, 2)
+        pgm.draw.rect(screen, taille_color, taille_rect, 2)
 
         # Afficher le texte capté
         dim_txt_surface = arialFont.render(dimentions, 1, (0, 0, 0))
         screen.blit(dim_txt_surface, (dim_rect.x + 5, dim_rect.y))
+        taille_txt_surface = arialFont.render(taille_pixels, 1, (0, 0, 0))
+        screen.blit(taille_txt_surface, (taille_rect.x + 5, taille_rect.y))
 
         # Elargir le rectangle au besoin
         dim_rect.w = max(100, dim_txt_surface.get_width() + 10)
-        # screen = pgm.display.set_mode(
-        #     (dimentions * taille_pixels, dimentions * taille_pixels + 50)
-        # )  # Afficher la fenetre
-        # # Dessin de la grille
-        # for y in range(len(grille)):
-        #     for x in range(len(grille[y])):
-        #         pgm.draw.rect(
-        #             screen,
-        #             (0, int(grille[y][x] * 255), int(grille[y][x] * 255)),
-        #             [
-        #                 x * taille_pixels,
-        #                 y * taille_pixels,
-        #                 taille_pixels,
-        #                 taille_pixels,
-        #             ],
-        #         )
+        taille_rect.w = max(100, taille_txt_surface.get_width() + 10)
 
-        # screen.blit(texte_compteur, (5, dimentions * taille_pixels + 5))
+        # Commencer la simulation
+        if jouer:
+            screen = pgm.display.set_mode(
+                (dimentions * taille_pixels, dimentions * taille_pixels + 50)
+            )  # Afficher la fenetre
+            for y in range(len(grille)):
+                for x in range(len(grille[y])):
+                    if grille[y][x] == 1:
+                        pgm.draw.rect(
+                            screen,
+                            (0, 0, 0),
+                            [
+                                x * taille_pixels,
+                                y * taille_pixels,
+                                taille_pixels,
+                                taille_pixels,
+                            ],
+                        )
+                    else:
+                        pgm.draw.rect(
+                            screen,
+                            (0, 0, 0),
+                            [
+                                x * taille_pixels,
+                                y * taille_pixels,
+                                taille_pixels,
+                                taille_pixels,
+                            ],
+                            1,
+                        )
+            screen.blit(text, (5, dimentions * taille_pixels + 5))
+    # Commencer la simulation
+    elif jouer:
+        screen = pgm.display.set_mode(
+            (dimentions * taille_pixels, dimentions * taille_pixels + 50)
+        )  # Afficher la fenetre
+        screen.fill((250, 250, 250))  # Changer la couleur de fond
+        # Parcourir la grille
+        for y in range(len(grille)):
+            for x in range(len(grille[y])):
+                if grille[y][x] == 1:
+                    pgm.draw.rect(
+                        screen,
+                        (0, 0, 0),
+                        [
+                            x * taille_pixels,
+                            y * taille_pixels,
+                            taille_pixels,
+                            taille_pixels,
+                        ],
+                    )
+                else:
+                    pgm.draw.rect(
+                        screen,
+                        (0, 0, 0),
+                        [
+                            x * taille_pixels,
+                            y * taille_pixels,
+                            taille_pixels,
+                            taille_pixels,
+                        ],
+                        1,
+                    )
+        screen.blit(gen, (5, dimentions * taille_pixels + 5))
     else:
         screen.blit(titre, (50, 50))
         screen.blit(play, (120, 150))
 
     pgm.display.flip()  # Actualisation de la fenetre
+    clock.tick(60)
